caf34bfd95a9a5451dd4817504ac62ce24058d16
Talha Roshaun <talharoshaun@gmail.com>
2022-06-24 12:48:36 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// create a state variable here
+     const [people , setPeople] = useState(data);
+     const [btn, setBtn] = useState('Clear All');
+	// this should clear all records
+	function clearAllRecords() {
+        setPeople(val => val === data ? [] : data)
+        setBtn (e => e === 'Clear All' ? e = 'Show All': e = "Clear All")
+    }
+	return (
+		<main>
+			<section className="container">
+				{/* change this to state variable data */}
+				<h3>{people.length} birthdays today</h3>
+				<List people={people} />
+				<button onClick={clearAllRecords}>{btn}</button>
+			</section>
+		</main>
+	)
 }
 
-export default App;
+export default App
